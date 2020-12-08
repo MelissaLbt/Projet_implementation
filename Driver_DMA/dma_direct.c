0345f7c009f3155fdc1ea969227fc33cc920769e
Melissa <lebretonmelissa.s@gmail.com>
2020-12-08 10:37:37 +0100

    Fin DMA_direct

--- Driver_DMA/dma_direct.c
+++ Driver_DMA/dma_direct.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-#DEFINE S2MM_DMACR_OFFSET 0x30
-#DEFINE S2MM_LENGTH_OFFSET 0x34
-#DEFINE S2MM_DMASR_OFFSET 0x58
 #DEFINE MM2S_DMACR_OFFSET 0x00
-#DEFINE MM2S_LENGTH_OFFSET 0x04
-#DEFINE MM2S_DMASR_OFFSET 0x28
+#DEFINE MM2S_DMASR_OFFSET 0x04
+#DEFINE MM2S_DA_OFFSET 0x18
+#DEFINE MM2S_LENGTH_OFFSET 0x28
+#DEFINE S2MM_DMACR_OFFSET 0x30
+#DEFINE S2MM_DA_OFFSET 0x48
+#DEFINE S2MM_LENGTH_OFFSET 0x58
+#DEFINE S2MM_DMASR_OFFSET 0x34
 
 struct axi_dma {
   volatile char* register_space;
@@ -19,37 +21,40 @@ struct axi_dma_buffer {
 };
 
 void read_dma_simple(struct axi_dma *dma, struct axi_dma_buffer *buffer){
-  // ioread32(dma->register_space);
+  iowrite32(dma->register_space + S2MM_DMACR_OFFSET, 0x1001);
+  iowrite32(buffer->size, dma->register_space + S2MM_LENGTH_OFFSET);
+  int reg = ioread32(dma->register_space + S2MM_DA_OFFSET);
+  iowrite32(buffer->data, reg);
+  iowrite32(dma->register_space + S2MM_DMASR_OFFSET, 0x1000);
 }
 
 void write_dma_simple(struct axi_dma *dma, struct axi_dma_buffer *buffer){
-  // iowrite32(dma->register_space, buffer->data);
+  iowrite32(dma->register_space + MM2S_DMACR_OFFSET, 0x1001);
+  iowrite32(dma->register_space + MMS2_LENGTH_OFFSET, buffer->size);
+  int reg = ioread32(buffer->data);
+  iowrite32(dma->register_space + MM2S_DA_OFFSET, reg);
+  iowrite32(dma->register_space + MM2S_DMASR_OFFSET, 0x1000);
 }
 
 void wait_rx_completion(struct axi_dma *dma) {
-  // struct axi_dma_buffer buffer;
-  // while(!dma->rx_done){
-  //   read_dma_simple(dma, buffer);
-  // }
-  // buffer->data = dma->register_space;
+  while(!dma->rx_done){
+    if(dma->register_space + S2MM_DMASR_OFFSET == 0x1000){
+      dma_irq_rx(dma);
+    }
+  }
 }
 
 void wait_tx_completion(struct axi_dma *dma) {
-  // struct axi_dma_buffer buffer;
-  // while(!dma->tx_done){
-  //   write_dma_simple(dma, buffer);
-  // }
+  while(!dma->tx_done){
+    if(dma->register_space + MM2S_DMASR_OFFSET == 0x1000){
+      dma_irq_tx(dma);
+    }
+  }
 }
 
 void dma_irq_rx(struct axi_dma *dma){
-  // if(S2MM_DMASR(12) == 1){
-  //   dma->rx_done = 1;
-  //   S2MM_DMASR(12) = 0;
-  }
+  dma->rx_done = 1;
 }
 void dma_irq_tx(struct axi_dma *dma){
-  // if(MM2S_DMASR(12) == 1){
-  //   dma->tx_done = 1;
-  //   MM2S_DMASR(12) = 0;
-  }
+  dma->tx_done = 1;
 }
