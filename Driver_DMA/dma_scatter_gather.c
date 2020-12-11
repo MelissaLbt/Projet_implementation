e03bc37a822ce74028ccffdc46bbc5181bf5294f
Melissa <lebretonmelissa.s@gmail.com>
2020-12-11 12:17:54 +0100

    Changement define

--- Driver_DMA/dma_scatter_gather.c
+++ Driver_DMA/dma_scatter_gather.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //Register space en mode scather-gather
-#DEFINE MM2S_DMACR_OFFSET 0x00
-#DEFINE MM2S_DMASR_OFFSET 0x04
-#DEFINE MM2S_CURDESC_OFFSET 0x08
+#define MM2S_DMACR_OFFSET 0x00
+#define MM2S_DMASR_OFFSET 0x04
+#define MM2S_CURDESC_OFFSET 0x08
 // #DEFINE MM2S_CURDESC_MSB_OFFSET 0x0C
-#DEFINE MM2S_TAILDESC_OFFSET 0x10
-#DEFINE S2MM_DMACR_OFFSET 0x30
-#DEFINE S2MM_DMASR_OFFSET 0x34
-#DEFINE S2MM_CURDESC_OFFSET 0x38
+#define MM2S_TAILDESC_OFFSET 0x10
+#define S2MM_DMACR_OFFSET 0x30
+#define S2MM_DMASR_OFFSET 0x34
+#define S2MM_CURDESC_OFFSET 0x38
 // #DEFINE S2MM_CURDESC_MSB_OFFSET 0x3C
-#DEFINE S2MM_TAILDESC_OFFSET 0x40
+#define S2MM_TAILDESC_OFFSET 0x40
 
 //Descripteur en mode scather-gather
-#DEFINE NEXTDESC_OFFSET 0x00
-// #DEFINE NEXTDESC_MSB_OFFSET 0x04
-#DEFINE BUFFER_ADDRESS_OFFSET 0x08
-// #DEFINE BUFFER_ADDRESS_MSB_OFFSET 0x0C
-// #DEFINE RESERVED_0_OFFSET 0x10
-// #DEFINE RESERVED_1_OFFSET 0x14
-// #DEFINE CONTROL_OFFSET 0x18
-// #DEFINE STATUS_OFFSET 0x1C
-// #DEFINE APP_0_OFFSET 0x20
-// #DEFINE APP_1_OFFSET 0x24
-// #DEFINE APP_2_OFFSET 0x28
-// #DEFINE APP_3_OFFSET 0x2C
-// #DEFINE APP_4_OFFSET 0x30
+#define NEXTDESC_OFFSET 0x00
+#define BUFFER_ADDRESS_OFFSET 0x08
 
 //Décalage de 64 octets
-#DEFINE OFFSET_64 0x40
+#define OFFSET_64 0x40
 
 struct axi_dma {
   volatile char* register_space;
@@ -44,7 +34,7 @@ struct axi_dma_buffer {
 struct axi_dma_sg {
   u32 nextdesc;
   u32 nextdesc_msb;
-  u32 buffer_address
+  u32 buffer_address;
   u32 buffer_address_msb;
   u32 reserved[2];
   u32 control;
@@ -54,21 +44,19 @@ struct axi_dma_sg {
 } __attribute__((aligned(64)));
 
 void dma_sg_init(struct axi_dma_sg *sg, struct axi_dma_buffer *buffer, size_t pkt_size){
-  size_t buffer_length = buffer->size;
+  int taille_max = (int) buffer->size / pkt_size;
   void *data = buffer->data;
-  int i = 0;
-  while(buffer_length >= pkt_size){
-    sg[i].nextdesc =  &s[i + 1];
+  for(int i = 0; i < taille_max; i++){
+    sg[i].nextdesc =  &sg[i + 1];
     sg[i].buffer_address = &data;
-    i++;
-    data += OFFSET_64;
-    buffer_length -= pkt_size;
+    iowrite32(sg->control, pkt_size);
+    data += pkt_size;
   }
 }
 
 void dma_sg_init_sparse(struct axi_dma_sg *sg, struct axi_dma_buffer **buffers, size_t num_buffers, size_t pkt_size){
   for(int i = 0; i < num_buffers; i++){
-    dma_sg_init(sg, buffer[i], pkt_size);
+    dma_sg_init(sg, buffers[i], pkt_size);
   }
 }
 
