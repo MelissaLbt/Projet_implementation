ba57e8e73ae96d0f736280681b322cea3edd1789
Melissa <lebretonmelissa.s@gmail.com>
2021-01-17 18:33:20 +0100

    Utilisation des macros ds start

--- polytecheise5driverdmatrou/dma.c
+++ polytecheise5driverdmatrou/dma.c
@@ -129,21 +129,21 @@ void axi_dma_buffer_release(struct axi_dma_channel *chan)
 void axi_dma_start(struct axi_dma_channel *chan)
 {
   // TODO
-  u32 sg=chan->sg_handles;
+  u32 sg = chan->sg_handles;
   if((chan->parent->has_tx) && AXI_DMA_IOC)
   {
-    iowrite32(1 | (1 << AXI_DMA_IOC), chan->parent->register_space + MM2S_CR); // on active le DMA et l'interruption IOC
-    iowrite32((u32)sg, chan->parent->register_space + MM2S_CD); // on écrit l'adresse du premier descripteur dans CURDESC
-    for(i = 0 ; (sg[i].control & (1 << EOF_BIT)) == 0 ; i++); // on cherche le dernier descripteur
-    iowrite32((u32)&sg[i-1], chan->parent->register_space + MM2S_TD); // on écrit l'adresse du dernier descripteur dans TAILDESC
+    iowrite32(1 | (1 << AXI_DMA_IOC), chan->parent->register_space + AXI_DMA_MM2S_DMACR); // on active le DMA et l'interruption IOC
+    iowrite32((u32)sg, chan->parent->register_space + AXI_DMA_MM2S_CURDESC); // on écrit l'adresse du premier descripteur dans CURDESC
+    for(i = 0 ; (sg[i].control & (1 << 26)) == 0 ; i++); // on cherche le dernier descripteur
+    iowrite32((u32)&sg[i-1], chan->parent->register_space + AXI_DMA_MM2S_TAILDESC); // on écrit l'adresse du dernier descripteur dans TAILDESC
   }
   if((chan->parent->has_rx) && AXI_DMA_IOC)
   {
-    u32 sg=chan->sg_handles;
-    iowrite32(1 | (1 << AXI_DMA_IOC), chan->parent->register_space + S2MM_CR); // on active le DMA et l'interruption IOC
-    iowrite32((u32)sg, chan->parent->register_space + S2MM_CD); // on écrit l'adresse du premier descripteur dans CURDESC
-    for(i = 0 ; (sg[i].control & (1 << EOF_BIT)) == 0 ; i++); // on cherche le dernier descripteur
-    iowrite32((u32)&sg[i], chan->parent->register_space + S2MM_TD); // on écrit l'adresse du dernier descripteur dans TAILDESC
+    sg = chan->sg_handles;
+    iowrite32(1 | (1 << AXI_DMA_IOC), chan->parent->register_space + AXI_DMA_S2MM_DMACR); // on active le DMA et l'interruption IOC
+    iowrite32((u32)sg, chan->parent->register_space + AXI_DMA_S2MM_CURDESC); // on écrit l'adresse du premier descripteur dans CURDESC
+    for(i = 0 ; (sg[i].control & (1 << 26)) == 0 ; i++); // on cherche le dernier descripteur
+    iowrite32((u32)&sg[i], chan->parent->register_space + S2MM_TAILDESC); // on écrit l'adresse du dernier descripteur dans TAILDESC
   }
 }
 
